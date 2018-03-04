428bb064984905479b7fa38b1ecc33a9045f0804
Vivek Kumar <vivek.kumar26@live.com>
2018-03-04 14:11:20 +0530

    refactor tic tac toe

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,167 +1,116 @@
-from random import randint
+'''TIC-TAC-TOE (Python 3.6)'''
+
+# Specifically for the iPython Notebook environment for clearing output
+from IPython.display import clear_output
+import random
+
+# Global variables
+theBoard = [' '] * 10  # a list of empty spaces
+available = [str(num) for num in range(0, 10)]  # a List Comprehension
+players = [0, 'X', 'O']  # note that players[1] == 'X' and players[-1] == 'O'
 
 
-# Function to display the board
-def display_board(board):
-    '''
-    Display the current board
-    Args: `board` -> List of values in current board
-    '''
-    # Clear the output
-    print('\n' * 50)
-    print('{}|{}|{}'.format(board[7], board[8], board[9]))
-    print('{}|{}|{}'.format(board[4], board[5], board[6]))
-    print('{}|{}|{}'.format(board[1], board[2], board[3]))
+def display_board(a, b):
+    '''Display the board'''
+    print('Available   TIC-TAC-TOE\n' + '  moves\n\n  ' + a[7] + '|' + a[8] +
+          '|' + a[9] + '        ' + b[7] + '|' + b[8] + '|' + b[9] + '\n  ' +
+          '-----        -----\n  ' + a[4] + '|' + a[5] + '|' + a[6] +
+          '        ' + b[4] + '|' + b[5] + '|' + b[6] + '\n  ' +
+          '-----        -----\n  ' + a[1] + '|' + a[2] + '|' + a[3] +
+          '        ' + b[1] + '|' + b[2] + '|' + b[3] + '\n')
 
 
-# Function to take in player input
-def player_input():
-    '''
-    OUTPUT = (Player 1 marker, Player 2 marker)
-    '''
-    marker = ''
-    while marker != 'X' and marker != 'O':
-        marker = input('Player 1: Choose X or O: ').upper()
-
-    if marker == 'X':
-        return ('X', 'O')
-    return ('O', 'X')
+display_board(available, theBoard)
 
 
-# Function to place marker on board
-def place_marker(board, marker, position):
-    '''
-    Function that takes in the board list object, a marker ('X' or 'O') and a
-    desired position (1-9) and assigns it to board
-    '''
+def place_marker(avail, board, marker, position):
+    '''Place marker on board'''
     board[position] = marker
+    avail[position] = ' '
 
 
-# Function to check if a player has won
 def win_check(board, mark):
-    '''
-    Function that takes in a board and a mark (X or O) and checks if that mark
-    has won
-    '''
-    rows = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
-    cols = [(1, 4, 7), (2, 5, 8), (3, 6, 9)]
-    diag = [(1, 5, 9), (3, 5, 7)]
-
-    # Check if all rows have same marker
-    for i, j, k in rows:
-        if board[i] == board[j] == board[k] == mark:
-            print('Mark {} has won!'.format(mark))
-            return True
-
-    # Check if all cols have same marker
-    for i, j, k in cols:
-        if board[i] == board[j] == board[k] == mark:
-            print('Mark {} has won!'.format(mark))
-            return True
-
-    # Check if all diagonals have same marker
-    for i, j, k in diag:
-        if board[i] == board[j] == board[k] == mark:
-            print('Mark {} has won!'.format(mark))
-            return True
-
-    return False
+    '''Check if any marker has won the game'''
+    return (
+        (board[7] == board[8] == board[9] == mark) or  # across the top
+        (board[4] == board[5] == board[6] == mark) or  # across the middle
+        (board[1] == board[2] == board[3] == mark) or  # across the bottom
+        (board[7] == board[4] == board[1] == mark) or  # down the middle
+        (board[8] == board[5] == board[2] == mark) or  # down the middle
+        (board[9] == board[6] == board[3] == mark) or  # down the right side
+        (board[7] == board[5] == board[3] == mark) or  # diagonal
+        (board[9] == board[5] == board[1] == mark))  # diagonal
 
 
-# Function to select which player goes first
-def choose_first():
-    '''
-    Function to randomly choose which player goes first
-    '''
-    flip = randint(0, 1)
-    if flip:
-        return 'Player 1'
-    return 'Player 2'
+def random_player():
+    '''Randomly choose who starts the game'''
+    return random.choice((-1, 1))
 
 
-# Function to check if a space is freely available on board
 def space_check(board, position):
-    '''
-    Function to check if space is freely available on board
-    '''
+    '''Check if a position is available on the board'''
     return board[position] == ' '
 
 
-# Function to check if board is full
-def is_board_full(board):
-    '''
-    Function to check if the board is full
-    '''
-    for i in range(1, 10):
-        if space_check(board, i):
-            return False
-    return True
+def full_board_check(board):
+    '''Check if board is completely filled'''
+    return ' ' not in board[1:]
 
 
-# Function that asks for player's next position
-def player_choice(board):
-    '''
-    Function that asks for a player's next position (1-9) and space_check() to
-    see if it's free
-    '''
+def player_choice(board, player):
+    '''Ask the player to mark his move'''
     position = 0
-    while position not in range(1, 10) or not space_check(board, position):
-        position = int(input('Choose a position: (1-9) '))
+
+    while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9
+                           ] or not space_check(board, position):
+        try:
+            position = int(
+                input('Player %s, choose your next position: (1-9) ' %
+                      (player)))
+        except:
+            print("I'm sorry, please try again.")
+
     return position
 
 
-# Function to check if a player wants to play again
 def replay():
-    choice = input('Play again? Enter Yes or No: ')
-    return choice.lower() == 'yes'
 
+    return input(
+        'Do you want to play again? Enter Yes or No: ').lower().startswith('y')
 
-# Run the game
-print('Welcome to TIC TAC TOE!')
-
+# Start the game
 while True:
-    the_board = [' '] * 10
-    player1_marker, player2_marker = player_input()
-    turn = choose_first()
-    print('{} will go first!'.format(turn))
+    clear_output()
+    print('Welcome to Tic Tac Toe!')
 
-    play_game = input('Ready to play? Y or N: ')
-    if play_game.lower() == 'y':
-        game_on = True
-    else:
-        game_on = False
+    toggle = random_player()
+    player = players[toggle]
+    print('For this round, Player %s will go first!' % (player))
 
+    game_on = True
+    input('Hit Enter to continue')
     while game_on:
-        if turn == 'Player 1':
-            display_board(the_board)
-            position = player_choice(the_board)
-            place_marker(the_board, player1_marker, position)
-            if win_check(the_board, player1_marker):
-                display_board(the_board)
-                print('PLAYER 1 HAS WON!')
-                game_on = False
-            else:
-                if is_board_full(the_board):
-                    display_board(the_board)
-                    print('TIE GAME!')
-                    game_on = False
-                else:
-                    turn = 'Player 2'
+        display_board(available, theBoard)
+        position = player_choice(theBoard, player)
+        place_marker(available, theBoard, player, position)
+
+        if win_check(theBoard, player):
+            display_board(available, theBoard)
+            print('Congratulations! Player ' + player + ' wins!')
+            game_on = False
         else:
-            display_board(the_board)
-            position = player_choice(the_board)
-            place_marker(the_board, player2_marker, position)
-            if win_check(the_board, player2_marker):
-                display_board(the_board)
-                print('PLAYER 2 HAS WON!')
-                game_on = False
+            if full_board_check(theBoard):
+                display_board(available, theBoard)
+                print('The game is a draw!')
+                break
             else:
-                if is_board_full(the_board):
-                    display_board(the_board)
-                    print('TIE GAME!')
-                    game_on = False
-                else:
-                    turn = 'Player 1'
+                toggle *= -1
+                player = players[toggle]
+                clear_output()
+
+    # reset the board and available moves list
+    theBoard = [' '] * 10
+    available = [str(num) for num in range(0, 10)]
 
     if not replay():
         break
